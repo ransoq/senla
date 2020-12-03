e4b7884c6de89838f93ee3e4fa284546bbcb1b72
Maxim <maximransov@gmail.com>
2020-12-03 12:42:54 +0300

    step 5

--- step 5/script.js
+++ step 5/script.js
@@ -110,15 +110,96 @@ const email = document.querySelector('#email'),
 
 email.onblur = function() {
     if (!email.value.includes('@')) {
-        email.focus();
-        email.classList.add('invalid');
-        error.innerHTML = 'Пожалуйста, введите правильный email.';
+      email.focus();
+      email.classList.add('invalid');
+      error.innerHTML = 'Пожалуйста, введите правильный email.';
     }
-};
+  };
   
-email.onfocus = function() {
+  email.onfocus = function() {
     if (this.classList.contains('invalid')) {
-        this.classList.remove('invalid');
-        error.innerHTML = "";
+      this.classList.remove('invalid');
+      error.innerHTML = "";
     }
+  };
+
+// ===================== Step 5
+
+// Arrow func
+
+let arrFunc = (a, b, c) => a + b - c;
+
+console.log(arrFunc(10, 10, 5));
+
+// default parametr 
+
+let defFunc = (a, b, c = 5) => a + b - c;
+
+console.log(defFunc(20, 20));
+
+// Spread/Rest operator
+
+const cats = ['Boris', 'Murzik', 'Barsik'],
+      dogs = ['Bobik', 'Sharik'],
+      animals = [...cats, ...dogs];
+
+console.log(animals);
+
+const nums = [1, 5, 4];
+
+function arrRest(a, b, c) {
+    return a + b + c;
+}
+
+console.log(arrRest(...nums));
+
+// class and super
+
+class Animal {
+    constructor(paws, tail, fur) {
+        this.paws = paws;
+        this.tail = tail;
+        this.fur = fur;
+    }
+    say() {
+        console.log(`Sorry, animals can't speak. But I can say I have ${this.paws} paws.`);
+    }
+}
+
+class Cat extends Animal {
+    constructor(paws, tail, fur) {
+        super(paws, tail, fur);
+        this.name = 'Boris';
+    }
+}
+
+const Boris = new Cat(4, true, true);
+
+Boris.say();
+console.log(Boris);
+
+//yield
+
+function* gen(i) {
+    while (i < 2) {
+      yield i;
+      i++;
+    }
+}
+
+const iter = gen(1);
+
+console.log(iter.next().value);
+console.log(iter.next().value);
+
+// Деструктуризация объекта
+
+const objDest = {
+    a: 1,
+    b: 2,
+    c: 3
 };
+
+const {a, b, c} = objDest;
+
+console.log(a, b, c);
